6d6bfd730eaa48566e226647cf151eed0ec7c7a8
noelia-alvez <68915999+noelia-alvez@users.noreply.github.com>
2020-08-13 22:22:47 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,6 +2,6 @@ function inicio (){
 
     var cont = localStorage.getItem("contador");
     if (cont == 0) {
-        window.location.href="login.html";
+        window.location.href="https://noelia-alvez.github.io/mi-primer-repositorio/login.html";
     }
 }
