f8a7190f1f09a661091d1c5380c0766ef8ea79a4
noelia-alvez <68915999+noelia-alvez@users.noreply.github.com>
2020-08-29 13:37:35 -0300

    Update products.js
    Se reutiliza código del archivo categories.js y se realizan modificaciones para cumplir con la pauta solicitada.
    Se modifican nombres para que utilice los datos del JSON PRODUCTS_URL.
    Se utiliza el JSON de PRODUCTS_URL.
    Se modifican funciones y se agregan condiciones.
    Se crea una variable búsqueda que guarda el contenido que trae de mi id="buscar" creado en products.html en el input type="text", luego se creó un evento keyup, esto hace que mi función se ejecute cada vez que se presiona una tecla, la función lo que hace es guardar mi valor tomado a través del id="buscar"  en la variable global creada llamada texto, lo convierte a minúsculas, y llama al método con showProductsList (productsArray).

--- js/products.js
+++ js/products.js
@@ -1,26 +1,26 @@
-var categoriesArray = [];
+var productsArray = [];
 
 
-function showCategoriesList(array){
+function showProductsList(array){
 
     let htmlContentToAppend = "";
     for(let i = 0; i < array.length; i++){
-        let category = array[i];
+        let products = array[i];
 
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
-                    <img src="` + category.imgSrc + `" alt=" " class="img-thumbnail">
+                    <img src="` + products.imgSrc + `" alt=" " class="img-thumbnail">
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">`+ category.name +`</h4>
-                        <small class="text-muted">` + category.soldCount + ` artículos</small>
-                        <p>` + category.cost + " " + category.currency + ` </p>
-                       
+                        <h4 class="mb-1">`+ products.name +`</h4>
+                            <p class="mb-1">` + products.soldCount + `</p>
+                            <p class="mb-1">` + products.cost + ` ` + products.currency + `</p>
+                            
                     </div>
-                     <p> ` + category.description + `<p>
+                     <p> ` + products.description + `<p>
 
                 </div>
             </div>
@@ -34,14 +34,169 @@ function showCategoriesList(array){
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCTS_URL).then(function(resultObj){
         if (resultObj.status == "ok")
         {
-            categoriesArray = resultObj.data;
-            //Muestro las categorías ordenadas
-            showCategoriesList(categoriesArray);
-            
+            productsArray = resultObj.data;
+            //Muestro los productos ordenados
+            showProductsList(productsArray);
         }
+    })
+})
+
+const ORDER_ASC_BY_PRICE = "$->$$";
+const ORDER_DESC_BY_PRICE = "$$->$";
+const ORDER_BY_PROD_REL = "RR->R";
+var currentProductsArray = [];
+var currentSortCriteria = undefined;
+var minCost = undefined;
+var maxCost = undefined;
+var texto = undefined;
+
+function sortProducts(criteria, array){
+    let result = [];
+    if (criteria === ORDER_ASC_BY_PRICE)
+    {
+        result = array.sort(function(a, b) { //función que ordena por precio de forma ascendente 
+            if ( a.cost < b.cost ){ return -1; }
+            if ( a.cost > b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_DESC_BY_PRICE){ //función que ordena por precio de forma descendente
+        result = array.sort(function(a, b) {
+            if ( a.cost > b.cost ){ return -1; }
+            if ( a.cost < b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_BY_PROD_REL){ //función que ordena por cantidad de vendidos
+        result = array.sort(function(a, b) {
+            let aCount = parseInt(a.soldCount); //se modifica nombre count por soldCount (JSON products)
+            let bCount = parseInt(b.soldCount);
+
+            if ( aCount > bCount ){ return -1; }
+            if ( aCount < bCount ){ return 1; }
+            return 0;
+        });
+    }
+
+    return result;
+}
+
+function showProductsList(){
+//se modifica para filtrar por precio
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let products = currentProductsArray[i];
+        let nombreProd = products.name.toLowerCase();
+        let descProd = products.description.toLowerCase();
+
+        if (
+            ((minCost == undefined) || (minCost != undefined && parseInt(products.cost) >= minCost)) &&
+            ((maxCost == undefined) || (maxCost != undefined && parseInt(products.cost) <= maxCost)) &&
+            ((descProd.indexOf(texto) !== -1 || (nombreProd.indexOf(texto) !== -1)) || (texto == undefined ))
+        ){
+// se modifica htmlContentToAppend para que coincida con LISTADO de productos
+                htmlContentToAppend += `
+                <div class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="` + products.imgSrc + `" alt=" " class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">`+ products.name +`</h4>
+                                    <p class="mb-1">` + products.soldCount + `</p>
+                                    <p class="mb-1">` + products.cost + ` ` + products.currency + `</p>
+                                    
+                            </div>
+                             <p> ` + products.description + `<p>
+        
+                        </div>
+                    </div>
+                </div>
+                `
+        }
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+function sortAndShowProducts(sortCriteria, productsArray){ //Función que ordena y muestra los productos
+    currentSortCriteria = sortCriteria; 
+
+    if(productsArray != undefined){
+        currentProductsArray = productsArray;
+    }
+
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+
+    //Muestro los productos ordenados
+    showProductsList();
+}
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){ //se cambia JSON categories por PRODUCTS_URL
+        if (resultObj.status === "ok"){
+            sortAndShowProducts(ORDER_ASC_BY_PRICE, resultObj.data);
+        }
+    });
+
+    document.getElementById("sortAsc").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_ASC_BY_PRICE);
+    });
+
+    document.getElementById("sortDesc").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_DESC_BY_PRICE);
+    });
+
+    document.getElementById("sortByCount").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_BY_PROD_REL);
+    });
+
+    document.getElementById("clearRangeFilter").addEventListener("click", function(){
+        document.getElementById("rangeFilterCostMin").value = "";
+        document.getElementById("rangeFilterCostMax").value = "";
+        document.getElementById("buscar").value = "";
+        texto = undefined;
+        minCost = undefined;
+        maxCost = undefined;
+      
+        showProductsList();
+    });
+
+    document.getElementById("rangeFilterCount").addEventListener("click", function(){
+        //Obtengo el mínimo y máximo de los intervalos para filtrar por precio
+        
+        minCost = document.getElementById("rangeFilterCostMin").value;
+        maxCost = document.getElementById("rangeFilterCostMax").value;
+
+        if ((minCost != undefined) && (minCost != "") && (parseInt(minCost)) >= 0){
+            minCost = parseInt(minCost);
+        }
+        else{
+            minCost = undefined;
+        }
+
+        if ((maxCost != undefined) && (maxCost != "") && (parseInt(maxCost)) >= 0){
+            maxCost = parseInt(maxCost);
+        }
+        else{
+            maxCost = undefined;
+        }
+        
+        showProductsList();
+    });
+
+
+        var busqueda = document.getElementById("buscar"); //Obtengo el id que nombre como buscar en el input de tipo text en products.html
+        busqueda.addEventListener("keyup", function(){ //Se ejecuta la función cada vez que se presiona una tecla
+        texto = busqueda.value.toLowerCase(); //Se convierte en minúsculas
+        showProductsList(productsArray); //Llama al método
     });
 });
