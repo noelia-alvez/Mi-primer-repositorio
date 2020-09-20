07872b52a9b3c994389805dcba025bd634c0f5c3
noelia-alvez <68915999+noelia-alvez@users.noreply.github.com>
2020-09-20 11:46:05 -0300

    Update product-info.js
    Se modificó función showImagesGallery para hacer uso de un carrusel para mostrar las mismas.-

--- js/product-info.js
+++ js/product-info.js
@@ -4,20 +4,50 @@ var prodRel = [];
 function showImagesGallery(array){
 
     let htmlContentToAppend = "";
-    for(let i = 0; i < array.length; i++){
-        let images = array[i];
+        let images = {
+            img1: array[0],
+            img2: array[1],
+            img3: array[2],
+            img4: array[3],
+            img5: array[4],
+        }
 
         htmlContentToAppend += `
-            <div class="col-lg-3 col-md-4 col-6">
-                <div class="d-block mb-4 h-100">
-                    <img class="img-fluid img-thumbnail" src="` + images + `" alt="">
-                </div>
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="` + images.img1 + `" class="d-block w-100" alt="...">
             </div>
+            <div class="carousel-item">
+                <img src="` + images.img2 + `" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="` + images.img3 + `" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="` + images.img4 + `" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="` + images.img5 + `" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
         `
-
-        document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
+        document.getElementById("carouselExampleIndicators").innerHTML = htmlContentToAppend;
     }
-}
 
 function showImagesGalleryRel(prodAll){
 
@@ -95,12 +125,9 @@ document.addEventListener("DOMContentLoaded", function(e){
             //Muestro las imagenes en forma de galería
             showImagesGallery(product.images);
         }
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function(e){
+    }
     getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok"){
+         if (resultObj.status === "ok"){
             products = resultObj.data;
             let prodRelacionados = document.getElementById("productImagesGalleryRel");
             prodRelacionados.innerHTML = products.imgSrc;
@@ -108,8 +135,6 @@ document.addEventListener("DOMContentLoaded", function(e){
         }
     });
 });
-
-document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
             prodComments = resultObj.data;
